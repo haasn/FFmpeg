a30e40688f6ee7009753bb6b48dae7d0178eeb91
Niklas Haas <git@haasn.dev>
2023-10-20 17:14:20 +0200

    XXXX fftools/ffmpeg_filter: configure buffersrc with csp/range
    Propagates input metadata to the input filter graph.
    
    FAILURES:
    - smjpeg

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -98,6 +98,8 @@ typedef struct InputFilterPriv {
 
     int width, height;
     AVRational sample_aspect_ratio;
+    enum AVColorSpace color_space;
+    enum AVColorRange color_range;
 
     int sample_rate;
     AVChannelLayout ch_layout;
@@ -118,6 +120,8 @@ typedef struct InputFilterPriv {
         int                 width;
         int                 height;
         AVRational          sample_aspect_ratio;
+        enum AVColorSpace   color_space;
+        enum AVColorRange   color_range;
 
         int                 sample_rate;
         AVChannelLayout     ch_layout;
@@ -216,6 +220,8 @@ static int sub2video_get_blank_frame(InputFilterPriv *ifp)
     frame->width  = ifp->width;
     frame->height = ifp->height;
     frame->format = ifp->format;
+    frame->colorspace = ifp->color_space;
+    frame->color_range = ifp->color_range;
 
     ret = av_frame_get_buffer(frame, 0);
     if (ret < 0)
@@ -810,6 +816,8 @@ static InputFilter *ifilter_alloc(FilterGraph *fg)
 
     ifp->format          = -1;
     ifp->fallback.format = -1;
+    ifp->color_space = ifp->fallback.color_space = AVCOL_SPC_UNSPECIFIED;
+    ifp->color_range = ifp->fallback.color_range = AVCOL_RANGE_UNSPECIFIED;
 
     ifp->frame_queue = av_fifo_alloc2(8, sizeof(AVFrame*), AV_FIFO_FLAG_AUTO_GROW);
     if (!ifp->frame_queue)
@@ -1414,11 +1422,8 @@ static int configure_input_video_filter(FilterGraph *fg, InputFilter *ifilter,
     int ret, pad_idx = 0;
     int64_t tsoffset = 0;
     AVBufferSrcParameters *par = av_buffersrc_parameters_alloc();
-
     if (!par)
         return AVERROR(ENOMEM);
-    memset(par, 0, sizeof(*par));
-    par->format = AV_PIX_FMT_NONE;
 
     if (ist->dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
         av_log(fg, AV_LOG_ERROR, "Cannot connect video filter to audio input\n");
@@ -1441,9 +1446,11 @@ static int configure_input_video_filter(FilterGraph *fg, InputFilter *ifilter,
     av_bprint_init(&args, 0, AV_BPRINT_SIZE_AUTOMATIC);
     av_bprintf(&args,
              "video_size=%dx%d:pix_fmt=%d:time_base=%d/%d:"
-             "pixel_aspect=%d/%d",
+             "pixel_aspect=%d/%d:colorspace=%s:range=%s",
              ifp->width, ifp->height, ifp->format,
-             ifp->time_base.num, ifp->time_base.den, sar.num, sar.den);
+             ifp->time_base.num, ifp->time_base.den, sar.num, sar.den,
+             av_color_space_name(ifp->color_space),
+             av_color_range_name(ifp->color_range));
     if (fr.num && fr.den)
         av_bprintf(&args, ":frame_rate=%d/%d", fr.num, fr.den);
     snprintf(name, sizeof(name), "graph %d input from stream %d:%d", fg->index,
@@ -1779,6 +1786,8 @@ int ifilter_parameters_from_dec(InputFilter *ifilter, const AVCodecContext *dec)
         ifp->fallback.width                  = dec->width;
         ifp->fallback.height                 = dec->height;
         ifp->fallback.sample_aspect_ratio    = dec->sample_aspect_ratio;
+        ifp->fallback.color_space            = dec->colorspace;
+        ifp->fallback.color_range            = dec->color_range;
     } else if (dec->codec_type == AVMEDIA_TYPE_AUDIO) {
         int ret;
 
@@ -1819,6 +1828,8 @@ static int ifilter_parameters_from_frame(InputFilter *ifilter, const AVFrame *fr
     ifp->width               = frame->width;
     ifp->height              = frame->height;
     ifp->sample_aspect_ratio = frame->sample_aspect_ratio;
+    ifp->color_space         = frame->colorspace;
+    ifp->color_range         = frame->color_range;
 
     ifp->sample_rate         = frame->sample_rate;
     ret = av_channel_layout_copy(&ifp->ch_layout, &frame->ch_layout);
@@ -2350,6 +2361,8 @@ int ifilter_send_eof(InputFilter *ifilter, int64_t pts, AVRational tb)
             ifp->width                  = ifp->fallback.width;
             ifp->height                 = ifp->fallback.height;
             ifp->sample_aspect_ratio    = ifp->fallback.sample_aspect_ratio;
+            ifp->color_space            = ifp->fallback.color_space;
+            ifp->color_range            = ifp->fallback.color_range;
 
             ret = av_channel_layout_copy(&ifp->ch_layout,
                                          &ifp->fallback.ch_layout);
@@ -2393,7 +2406,9 @@ int ifilter_send_frame(InputFilter *ifilter, AVFrame *frame, int keep_reference)
         break;
     case AVMEDIA_TYPE_VIDEO:
         need_reinit |= ifp->width  != frame->width ||
-                       ifp->height != frame->height;
+                       ifp->height != frame->height ||
+                       ifp->color_space != frame->colorspace ||
+                       ifp->color_range != frame->color_range;
         break;
     }
 
