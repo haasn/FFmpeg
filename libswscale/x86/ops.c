90ccf066d85ccf25d9805da5b6056b22b5ca9439
Niklas Haas <git@haasn.dev>
2025-03-19 02:54:22 +0100

    (WIP) swscale/x86: add SIMD backend

---
+++ libswscale/x86/ops.c
@@ -0,0 +1,277 @@
+/**
+ * Copyright (C) 2025 Niklas Haas
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "../ops_internal.h"
+
+#define DECL_ASM(TYPE, NAME, ...)                                               \
+    void ff_##NAME(const SwsOpExec *, const SwsOpImpl *);                       \
+    static const SwsOpEntry op_##NAME = {                                       \
+        .func = ff_##NAME,                                                      \
+        .op.type = SWS_PIXEL_##TYPE,                                            \
+        __VA_ARGS__                                                             \
+    }
+
+#define DECL_PATTERN(TYPE, NAME, X, Y, Z, W, ...)                               \
+    DECL_ASM(TYPE, p##X##Y##Z##W##_##NAME,                                      \
+        .op.comps.unused = { !X, !Y, !Z, !W },                                  \
+        __VA_ARGS__                                                             \
+    )
+
+#define REF_PATTERN(NAME, X, Y, Z, W)                                           \
+    op_p##X##Y##Z##W##_##NAME
+
+#define DECL_COMMON_PATTERNS(TYPE, NAME, ...)                                   \
+    DECL_PATTERN(TYPE, NAME, 1, 0, 0, 0, __VA_ARGS__);                          \
+    DECL_PATTERN(TYPE, NAME, 1, 0, 0, 1, __VA_ARGS__);                          \
+    DECL_PATTERN(TYPE, NAME, 1, 1, 1, 0, __VA_ARGS__);                          \
+    DECL_PATTERN(TYPE, NAME, 1, 1, 1, 1, __VA_ARGS__)                           \
+
+#define REF_COMMON_PATTERNS(NAME)                                               \
+    REF_PATTERN(NAME, 1, 0, 0, 0),                                              \
+    REF_PATTERN(NAME, 1, 0, 0, 1),                                              \
+    REF_PATTERN(NAME, 1, 1, 1, 0),                                              \
+    REF_PATTERN(NAME, 1, 1, 1, 1)
+
+#define DECL_RW(EXT, NAME, OP, ELEMS, PACKED)                                   \
+    DECL_ASM(U8, NAME##ELEMS##EXT,                                              \
+        .op.op = SWS_OP_##OP,                                                   \
+        .op.rw = { .elems = ELEMS, .packed = PACKED },                          \
+    );
+
+#define DECL_CLEAR_ALPHA(EXT, NAME, IDX)                                        \
+    DECL_ASM(U8, clear_alpha##IDX##EXT,                                         \
+        .op.op = SWS_OP_CLEAR,                                                  \
+        .op.clear.value[IDX] = { .num = 0xFF, .den = 1 },                       \
+        .op.comps.unused[IDX] = true,                                           \
+    );
+
+#define DECL_SWIZZLE(EXT, X, Y, Z, W)                                           \
+    DECL_ASM(U8, swizzle_##X##Y##Z##W##EXT,                                     \
+        .op.op = SWS_OP_SWIZZLE,                                                \
+        .op.swizzle = SWS_SWIZZLE( X, Y, Z, W ),                                \
+    );
+
+#define DECL_FUNCS_8(EXT)                                                       \
+    DECL_RW(EXT, read_planar, READ, 1, false)                                   \
+    DECL_RW(EXT, read_planar, READ, 2, false)                                   \
+    DECL_RW(EXT, read_planar, READ, 3, false)                                   \
+    DECL_RW(EXT, read_planar, READ, 4, false)                                   \
+    DECL_RW(EXT, write_planar, WRITE, 1, false)                                 \
+    DECL_RW(EXT, write_planar, WRITE, 2, false)                                 \
+    DECL_RW(EXT, write_planar, WRITE, 3, false)                                 \
+    DECL_RW(EXT, write_planar, WRITE, 4, false)                                 \
+    DECL_RW(EXT, read8_packed, READ, 2, true)                                   \
+    DECL_SWIZZLE(EXT, 3, 0, 1, 2)                                               \
+    DECL_SWIZZLE(EXT, 2, 1, 0, 3)                                               \
+    DECL_SWIZZLE(EXT, 3, 2, 1, 0)                                               \
+    DECL_SWIZZLE(EXT, 3, 1, 0, 2)                                               \
+    DECL_SWIZZLE(EXT, 3, 2, 0, 1)                                               \
+    DECL_SWIZZLE(EXT, 1, 2, 0, 3)                                               \
+    DECL_SWIZZLE(EXT, 1, 0, 2, 3)                                               \
+    DECL_SWIZZLE(EXT, 2, 0, 1, 3)                                               \
+    DECL_SWIZZLE(EXT, 2, 3, 1, 0)                                               \
+    DECL_SWIZZLE(EXT, 2, 1, 3, 0)                                               \
+    DECL_SWIZZLE(EXT, 1, 2, 3, 0)                                               \
+    DECL_SWIZZLE(EXT, 0, 2, 1, 3)                                               \
+    DECL_SWIZZLE(EXT, 0, 2, 3, 1)                                               \
+    DECL_SWIZZLE(EXT, 0, 3, 1, 2)                                               \
+    DECL_SWIZZLE(EXT, 3, 1, 2, 0)                                               \
+    DECL_SWIZZLE(EXT, 0, 3, 2, 1)                                               \
+    DECL_SWIZZLE(EXT, 0, 0, 0, 3)                                               \
+    DECL_SWIZZLE(EXT, 3, 0, 0, 0)                                               \
+    DECL_SWIZZLE(EXT, 0, 0, 0, 1)                                               \
+    DECL_SWIZZLE(EXT, 1, 0, 0, 0)                                               \
+    DECL_CLEAR_ALPHA(EXT, clear_alpha, 0)                                       \
+    DECL_CLEAR_ALPHA(EXT, clear_alpha, 1)                                       \
+    DECL_CLEAR_ALPHA(EXT, clear_alpha, 3)
+
+#define DECL_CONVERT(EXT, FROM, TO)                                             \
+    DECL_COMMON_PATTERNS(U##FROM, convert_##FROM##to##TO##EXT,                  \
+        .op.op = SWS_OP_CONVERT,                                                \
+        .op.convert.to = SWS_PIXEL_U##TO,                                       \
+    );
+
+static int setup_shift(const SwsOp *op, SwsOpPriv *out)
+{
+    out->u16[0] = op->shift.amount;
+    return 0;
+}
+
+#define DECL_SHIFT16(EXT)                                                       \
+    DECL_COMMON_PATTERNS(U16, lshift16##EXT,                                    \
+        .op.op = SWS_OP_LSHIFT,                                                 \
+        .setup = setup_shift,                                                   \
+    );                                                                          \
+                                                                                \
+    DECL_COMMON_PATTERNS(U16, rshift16##EXT,                                    \
+        .op.op = SWS_OP_RSHIFT,                                                 \
+        .setup = setup_shift,                                                   \
+    );
+
+#define DECL_FUNCS_16(EXT)                                                      \
+    DECL_CONVERT(EXT,  8, 16)                                                   \
+    DECL_CONVERT(EXT, 16,  8)                                                   \
+    DECL_SHIFT16(EXT)
+
+#define REF_OPS_U8(EXT)      \
+    op_read_planar1##EXT,    \
+    op_read_planar2##EXT,    \
+    op_read_planar3##EXT,    \
+    op_read_planar4##EXT,    \
+    op_write_planar1##EXT,   \
+    op_write_planar2##EXT,   \
+    op_write_planar3##EXT,   \
+    op_write_planar4##EXT,   \
+    /*op_read8_packed2##EXT,*/   \
+    op_swizzle_3012##EXT,    \
+    op_swizzle_0003##EXT,    \
+    op_swizzle_0001##EXT,    \
+    op_swizzle_3000##EXT,    \
+    op_swizzle_1000##EXT,    \
+    op_clear_alpha0##EXT,    \
+    op_clear_alpha1##EXT,    \
+    op_clear_alpha3##EXT,
+
+#define REF_OPS_U16(EXT)                        \
+    REF_COMMON_PATTERNS(convert_8to16##EXT),    \
+    REF_COMMON_PATTERNS(convert_16to8##EXT),    \
+    REF_COMMON_PATTERNS(lshift16##EXT),         \
+    REF_COMMON_PATTERNS(rshift16##EXT),
+
+DECL_FUNCS_8(_m1_sse2)
+DECL_FUNCS_8(_m1_avx2)
+DECL_FUNCS_8(_m2_avx2)
+
+DECL_FUNCS_16(_m1_avx2)
+DECL_FUNCS_16(_m2_avx2)
+
+static const SwsOpTable ops_u8_m1_sse2 = {
+    .cpu_flags = AV_CPU_FLAG_SSE2,
+    .block_w = 16,
+    .block_h = 1,
+    .entries = {
+        REF_OPS_U8(_m1_sse2)
+        {{0}}
+    },
+};
+
+static const SwsOpTable ops_u8_m1_avx2 = {
+    .cpu_flags = AV_CPU_FLAG_AVX2,
+    .block_w = 32,
+    .block_h = 1,
+    .entries = {
+        REF_OPS_U8(_m1_avx2)
+        {{0}}
+    },
+};
+
+static const SwsOpTable ops_u8_m2_avx2 = {
+    .cpu_flags = AV_CPU_FLAG_AVX2,
+    .block_w = 64,
+    .block_h = 1,
+    .entries = {
+        REF_OPS_U8(_m2_avx2)
+        {{0}}
+    },
+};
+
+static const SwsOpTable ops_u16_m1_avx2 = {
+    .cpu_flags = AV_CPU_FLAG_AVX2,
+    .block_w = 16,
+    .block_h = 1,
+    .entries = {
+        REF_OPS_U16(_m1_avx2)
+        {{0}}
+    },
+};
+
+static const SwsOpTable ops_u16_m2_avx2 = {
+    .cpu_flags = AV_CPU_FLAG_AVX2,
+    .block_w = 32,
+    .block_h = 1,
+    .entries = {
+        REF_OPS_U16(_m2_avx2)
+        {{0}}
+    },
+};
+
+static av_const int get_mmsize(void)
+{
+    const int cpu_flags = av_get_cpu_flags();
+    if (cpu_flags & AV_CPU_FLAG_AVX2)
+        return 32;
+    else if (cpu_flags & AV_CPU_FLAG_SSE2)
+        return 16;
+    else
+        return AVERROR(ENOTSUP);
+}
+
+/**
+ * Returns true if the operation's implementation only depends on the block
+ * size, and not the underlying pixel type
+ */
+static int op_is_type_invariant(const SwsOp *op)
+{
+    switch (op->op) {
+    case SWS_OP_READ:
+    case SWS_OP_WRITE:
+        return !op->rw.packed && !op->rw.frac;
+    case SWS_OP_SWIZZLE:
+        return true;
+    }
+
+    return false;
+}
+
+static int compile(SwsOpList *ops, SwsOpChain *chain)
+{
+    int ret;
+
+    static const SwsOpTable *const tables[] = {
+        &ops_u8_m1_sse2,
+        &ops_u8_m1_avx2,
+        &ops_u8_m2_avx2,
+        &ops_u16_m1_avx2,
+        &ops_u16_m2_avx2,
+    };
+
+    /* Use at most two full vregs during the widest precision section */
+    chain->block_w = 2 * get_mmsize() / ff_sws_op_list_max_size(ops);
+    chain->block_h = 1;
+
+    do {
+        int block_w = chain->block_w, block_h = chain->block_h;
+        SwsOp *op = &ops->ops[0];
+
+        if (op_is_type_invariant(op)) {
+            block_w *= ff_sws_pixel_type_size(op->type);
+            op->type = SWS_PIXEL_U8;
+        }
+
+        ret = ff_sws_op_compile_tables(tables, FF_ARRAY_ELEMS(tables), ops,
+                                       block_w, block_h, chain);
+    } while (ret == AVERROR(EAGAIN));
+    return ret;
+}
+
+SwsOpBackend backend_x86 = {
+    .name       = "x86",
+    .compile    = compile,
+};
---
+++ libswscale/x86/ops_int.asm
@@ -0,0 +1,414 @@
+;******************************************************************************
+;* Copyright (c) 2025 Niklas Haas
+;*
+;* This file is part of FFmpeg.
+;*
+;* FFmpeg is free software; you can redistribute it and/or
+;* modify it under the terms of the GNU Lesser General Public
+;* License as published by the Free Software Foundation; either
+;* version 2.1 of the License, or (at your option) any later version.
+;*
+;* FFmpeg is distributed in the hope that it will be useful,
+;* but WITHOUT ANY WARRANTY; without even the implied warranty of
+;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;* Lesser General Public License for more details.
+;*
+;* You should have received a copy of the GNU Lesser General Public
+;* License along with FFmpeg; if not, write to the Free Software
+;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+;******************************************************************************
+
+%include "libavutil/x86/x86util.asm"
+
+SECTION .text
+
+struc SwsOpExec
+    .in0 resq 1
+    .in1 resq 1
+    .in2 resq 1
+    .in3 resq 1
+    .out0 resq 1
+    .out1 resq 1
+    .out2 resq 1
+    .out3 resq 1
+endstruc
+
+struc SwsOpImpl
+    .cont resb 16
+    .priv resb 16
+    .next resb 0
+endstruc
+
+; common macros for declaring operations
+%macro DEF_OP_NAME 1 ; name
+    %ifdef X
+        %define ADD_PAT(name) p %+ X %+ Y %+ Z %+ W %+ _ %+ name
+    %else
+        %define ADD_PAT(name) name
+    %endif
+
+    %ifdef V2
+        %if V2
+            %define ADD_MUL(name) name %+ _m2
+        %else
+            %define ADD_MUL(name) name %+ _m1
+        %endif
+    %else
+        %define ADD_MUL(name) name
+    %endif
+
+    %xdefine NAME ADD_PAT(ADD_MUL(%1))
+    %undef ADD_PAT
+    %undef ADD_MUL
+%endmacro
+
+%macro op 1 ; name
+    DEF_OP_NAME %1
+    cglobal NAME, 2, 7, 16, exec, impl
+    %undef NAME
+%endmacro
+
+%macro decl_v2 2+ ; v2, func
+    %xdefine V2 %1
+    %2
+    %undef V2
+%endmacro
+
+%macro decl_pattern 5+ ; X, Y, Z, W, func
+    %xdefine X %1
+    %xdefine Y %2
+    %xdefine Z %3
+    %xdefine W %4
+    %5
+    %undef X
+    %undef Y
+    %undef Z
+    %undef W
+%endmacro
+
+%macro decl_common_patterns 1+ ; func
+    decl_pattern 1, 0, 0, 0, %1 ; y
+    decl_pattern 1, 0, 0, 1, %1 ; ya
+    decl_pattern 1, 1, 1, 0, %1 ; yuv
+    decl_pattern 1, 1, 1, 1, %1 ; yuva
+%endmacro
+
+; common names for the internal vector calling convention
+%define  mx    m0
+%define  my    m1
+%define  mz    m2
+%define  mw    m3
+%define xmx   xm0
+%define xmy   xm1
+%define xmz   xm2
+%define xmw   xm3
+%define ymx   ym0
+%define ymy   ym1
+%define ymz   ym2
+%define ymw   ym3
+
+%define  mx2   m4
+%define  my2   m5
+%define  mz2   m6
+%define  mw2   m7
+%define xmx2  xm4
+%define xmy2  xm5
+%define xmz2  xm6
+%define xmw2  xm7
+%define ymx2  ym4
+%define ymy2  ym5
+%define ymz2  ym6
+%define ymw2  ym7
+
+; load the next operation kernel
+%macro LOAD_CONT 1
+    mov %1, [implq + SwsOpImpl.cont]
+%endmacro
+
+; tail call into the next operation kernel
+%macro CONTINUE 1
+    add implq, SwsOpImpl.next
+    jmp %1
+    annotate_function_size
+%endmacro
+
+; return from operations chain after write
+%macro END 0
+    ; always force a vzeroupper before returning from the last function
+    %if !vzeroupper_required
+        vzeroupper
+    %endif
+    RET
+%endmacro
+
+; helper for inline conditionals
+%macro IF 2+ ; cond, body
+    %if %1
+        %2
+    %endif
+%endmacro
+
+;---------------------------------------------------------
+; Planar reads / writes
+
+%macro read_planar 1 ; elems
+op read_planar%1
+            mov r2, [execq + SwsOpExec.in0]
+IF %1 > 1,  mov r3, [execq + SwsOpExec.in1]
+IF %1 > 2,  mov r4, [execq + SwsOpExec.in2]
+IF %1 > 3,  mov r5, [execq + SwsOpExec.in3]
+            LOAD_CONT r6
+            movu mx, [r2]
+IF %1 > 1,  movu my, [r3]
+IF %1 > 2,  movu mz, [r4]
+IF %1 > 3,  movu mw, [r5]
+%if V2
+            movu mx2, [r2 + mmsize]
+IF %1 > 1,  movu my2, [r3 + mmsize]
+IF %1 > 2,  movu mz2, [r4 + mmsize]
+IF %1 > 3,  movu mw2, [r5 + mmsize]
+%endif
+            CONTINUE r6
+%endmacro
+
+%macro write_planar 1 ; elems
+op write_planar%1
+            mov r1, [execq + SwsOpExec.out0]
+IF %1 > 1,  mov r2, [execq + SwsOpExec.out1]
+IF %1 > 2,  mov r3, [execq + SwsOpExec.out2]
+IF %1 > 3,  mov r4, [execq + SwsOpExec.out3]
+            movu [r1], mx
+IF %1 > 1,  movu [r2], my
+IF %1 > 2,  movu [r3], mz
+IF %1 > 3,  movu [r4], mw
+%if V2
+            movu [r1 + mmsize], mx2
+IF %1 > 1,  movu [r2 + mmsize], my2
+IF %1 > 2,  movu [r3 + mmsize], mz2
+IF %1 > 3,  movu [r4 + mmsize], mw2
+%endif
+            END
+%endmacro
+
+%macro read8_packed 0
+op read8_packed2
+    mov r2, [execq + SwsOpExec.in0]
+    LOAD_CONT r3
+    movu mx, [r2]               ; YAYA low
+    movu mw, [r2 + mmsize]      ; YAYA high
+    pcmpeqb mz, mz, mz          ; FFFF
+    psrlw mz, mz, 8             ; F0F0
+    pand m4, mx, mz             ; Y0Y0 low
+    pand m5, mw, mz             ; Y0Y0 high
+    psrlw my, mx, 8             ; A0A0 low
+    psrlw m6, m6, 8             ; A0A0 high
+    packuswb mx, m4, m5         ; YYYY low+high
+    packuswb my, my, m6         ; AAAA low+high
+%if V2
+;    movu m3, [r2 + mmsize*2]
+;    movu m6, [r2 + mmsize*3]
+;    pand m7, m2, m3 ; Y0Y0
+;    pand m8, m2, m6 ; Y0Y0
+;    psrlw m3, m3, 8 ; A0A0
+;    psrlw m6, m6, 8 ; A0A0
+;    packuswb mx2, m7, m8 ; YYYY
+;    packuswb my2, m3, m6 ; AAAA
+%endif
+    %if avx_enabled
+    vpermq mx, mx, q0213
+    vpermq my, my, q0213
+    %endif
+    CONTINUE r3
+%endmacro
+
+;---------------------------------------------------------
+; Clearing
+
+%macro clear_alpha 3 ; idx, vreg, vreg2
+op clear_alpha%1
+        LOAD_CONT r2
+        pcmpeqb %2, %2, %2
+IF V2,  mova %3, %2
+        CONTINUE r2
+%endmacro
+
+;---------------------------------------------------------
+; Swizzling and duplicating
+
+; mA := mB, mB := mC, ... mX := mA
+%macro vrotate 2-* ; A, B, C, ...
+    %rep %0
+        %assign a %1 + 4
+        %assign b %2 + 4
+        mova m%1, m%2
+        IF V2, mova m%[a], m%[b]
+    %rotate 1
+    %endrep
+%endmacro
+
+%macro swizzle_funcs 0
+op swizzle_3012
+    LOAD_CONT r2
+    vrotate 8, 0, 3, 2, 1
+    CONTINUE r2
+
+op swizzle_0003
+    LOAD_CONT r2
+    mova my, mx
+    mova mz, mx
+%if V2
+    mova my2, mx2
+    mova mz2, mx2
+%endif
+    CONTINUE r2
+
+op swizzle_0001
+    LOAD_CONT r2
+    mova mw, my
+    mova mz, mx
+    mova my, mx
+%if V2
+    mova mw2, my2
+    mova mz2, mx2
+    mova my2, mx2
+%endif
+    CONTINUE r2
+
+op swizzle_3000
+    LOAD_CONT r2
+    mova my, mx
+    mova mz, mx
+    mova mx, mw
+    mova mw, my
+%if V2
+    mova my2, mx2
+    mova mz2, mx2
+    mova mx2, mw2
+    mova mw2, my2
+%endif
+    CONTINUE r2
+
+op swizzle_1000
+    LOAD_CONT r2
+    mova mz, mx
+    mova mw, mx
+    mova mx, my
+    mova my, mz
+%if V2
+    mova mz2, mx2
+    mova mw2, mx2
+    mova mx2, my2
+    mova my2, mz2
+%endif
+    CONTINUE r2
+%endmacro
+
+;---------------------------------------------------------
+; Pixel type conversions
+
+%macro conv8to16 0
+op convert_8to16
+        LOAD_CONT r2
+%if V2
+IF X,   vextracti128 xmx2, ymx, 1
+IF Y,   vextracti128 xmy2, ymy, 1
+IF Z,   vextracti128 xmz2, ymz, 1
+IF W,   vextracti128 xmw2, ymw, 1
+IF X,   pmovzxbw ymx2, xmx2
+IF Y,   pmovzxbw ymy2, xmy2
+IF Z,   pmovzxbw ymz2, xmz2
+IF W,   pmovzxbw ymw2, xmw2
+%endif
+IF X,   pmovzxbw ymx, xmx
+IF Y,   pmovzxbw ymy, xmy
+IF Z,   pmovzxbw ymz, xmz
+IF W,   pmovzxbw ymw, xmw
+        CONTINUE r2
+%endmacro
+
+%macro conv16to8 0
+op convert_16to8
+        LOAD_CONT r2
+IF X,   vextracti128  xm8, ymx, 1
+IF Y,   vextracti128  xm9, ymy, 1
+IF Z,   vextracti128 xm10, ymz, 1
+IF W,   vextracti128 xm11, ymw, 1
+IF X,   packuswb xmx, xmx, xm8
+IF Y,   packuswb xmy, xmy, xm9
+IF Z,   packuswb xmz, xmz, xm10
+IF W,   packuswb xmw, xmw, xm11
+        ; TODO: V2
+        CONTINUE r2
+%endmacro
+
+;---------------------------------------------------------
+; Shifting
+
+%macro lshift16 0
+op lshift16
+        vmovq xm8, [implq + SwsOpImpl.priv]
+        LOAD_CONT r2
+IF X,   psllw mx, mx, xm8
+IF Y,   psllw my, my, xm8
+IF Z,   psllw mz, mz, xm8
+IF W,   psllw mw, mw, xm8
+%if V2
+IF X,   psllw mx2, mx2, xm8
+IF Y,   psllw my2, my2, xm8
+IF Z,   psllw mz2, mz2, xm8
+IF W,   psllw mw2, mw2, xm8
+%endif
+        CONTINUE r2
+%endmacro
+
+%macro rshift16 0
+op rshift16
+        vmovq xm8, [implq + SwsOpImpl.priv]
+        LOAD_CONT r2
+IF X,   psrlw mx, mx, xm8
+IF Y,   psrlw my, my, xm8
+IF Z,   psrlw mz, mz, xm8
+IF W,   psrlw mw, mw, xm8
+%if V2
+IF X,   psrlw mx2, mx2, xm8
+IF Y,   psrlw my2, my2, xm8
+IF Z,   psrlw mz2, mz2, xm8
+IF W,   psrlw mw2, mw2, xm8
+%endif
+        CONTINUE r2
+%endmacro
+
+;---------------------------------------------------------
+; Function instantiations
+
+%macro funcs_u8 0
+    read_planar 1
+    read_planar 2
+    read_planar 3
+    read_planar 4
+    read8_packed
+    write_planar 1
+    write_planar 2
+    write_planar 3
+    write_planar 4
+    clear_alpha 0, mx, mx2
+    clear_alpha 1, my, my2
+    clear_alpha 3, mw, mw2
+    swizzle_funcs
+%endmacro
+
+%macro funcs_u16 0
+    decl_common_patterns conv8to16
+    decl_common_patterns conv16to8
+    decl_common_patterns lshift16
+    decl_common_patterns rshift16
+%endmacro
+
+INIT_XMM sse2
+decl_v2 0, funcs_u8
+
+INIT_YMM avx2
+decl_v2 0, funcs_u8
+decl_v2 1, funcs_u8
+decl_v2 0, funcs_u16
+decl_v2 1, funcs_u16
